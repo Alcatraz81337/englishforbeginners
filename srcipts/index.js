47e3a98085ab8569050f782a9dcf5ee47af052ba
Vladislav <infohunterr@gmail.com>
2021-01-11 11:36:01 +0600

    fixes

--- srcipts/index.js
+++ srcipts/index.js
@@ -1,7 +1,5 @@
-// import { musicPlayerInit } from "./musicPlayer.js";
 import { videoPlayerInit } from "./videoPlayer.js";
 import { videoPlayerInit2 } from "./videoPlayer2.js";
 
-// musicPlayerInit();
 videoPlayerInit();
 videoPlayerInit2();
